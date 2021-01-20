74ccc03e6c46b2ea802c7a1c7e1d02c05c5546e5
Mannan Shukla <58566668+mannanshukla@users.noreply.github.com>
2021-01-20 10:38:14 -0500

    Add files via upload

---
+++ PrintStars.java
@@ -0,0 +1,16 @@
+/*
+ * mannan shukla
+ * making a 10 x 5 box of *s
+ * december 21st 2020
+ */
+
+public class PrintStars{
+    public static void main(String args[]){
+        for(int i = 0; i <= 4; i++){
+            for(int j = 0; j <= 9; j++){
+                System.out.print("*");
+            }
+        System.out.println("");
+    }
+}
+}
