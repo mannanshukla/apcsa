005915d99476d6fd77e5e412288ebe78df9af91a
monado <monado@snopyta.org>
2021-01-22 11:31:56 -0500

    added yet another do now

--- ch8/DoNow.java
+++ ch8/DoNow.java
@@ -1,15 +1,29 @@
-/*
- *mannan shukla
- *january 20th 2021
- *do now testing strings and return types
- */
-
 public class DoNow{
-    public static String test(){
-        int x = 4;
-        return Integer.toString(x);
-    }
-    public static void main(String args[]){
-        System.out.println(test());
-    }
+
+     
+     public static boolean isEmail(String str){
+            if(str.contains("@") && str.contains(".")){
+                if(str.indexOf("@") < str.lastIndexOf(".")){
+                    if(str.charAt(0) != '@'){
+                        return true;
+                    }
+                    else{return false;}
+                }
+                else{return false;}
+            }
+            else{return false;}
+            
+        }
+        
+     public static void main(String []args){
+         
+        
+        
+        System.out.println(isEmail("abc@yahoo.com")); // valid
+        System.out.println(isEmail("a.bc@gmail.com")); // valid
+        System.out.println(isEmail("a.bc@msncom")); // not valid
+        System.out.println(isEmail("abc@yahoocom")); // not valid
+        System.out.println(isEmail("@yahoo.com")); // not valid
+        System.out.println(isEmail("abcyahoo.com")); // not valid 
+     }
 }
